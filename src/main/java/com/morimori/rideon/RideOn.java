6eecc2ab47b3f4e267d135c792373bec9bafac08
MORIMORI0317 <43478594+MORIMORI0317@users.noreply.github.com>
2020-01-05 11:28:06 +0900

    1.15.1-1.1

--- src/main/java/com/morimori/rideon/RideOn.java
+++ src/main/java/com/morimori/rideon/RideOn.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class RideOn {
 
 	public static final String MODID = "rideon";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(),
 			() -> () -> new ServerProxy());
 
